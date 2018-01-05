175c2a86f68a8aaceb4d260a218fb94bf62bbcda
Alexander Jellymath <Jellymath@outlook.com>
2018-01-06 02:08:13 +0300

    Support domain names, add list operation support, provide support of photo and link attachments, basic support of video and audio attachments.

--- src/main/kotlin/VkFetcherBot.kt
+++ src/main/kotlin/VkFetcherBot.kt
@@ -1,19 +1,21 @@
 import com.github.salomonbrys.kotson.fromJson
 import com.google.gson.Gson
-import org.telegram.telegrambots.ApiContextInitializer
-import org.telegram.telegrambots.TelegramBotsApi
-import org.telegram.telegrambots.api.objects.Update
-import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import com.vk.api.sdk.client.VkApiClient
 import com.vk.api.sdk.client.actors.ServiceActor
 import com.vk.api.sdk.httpclient.HttpTransportClient
 import com.vk.api.sdk.objects.wall.WallpostFull
 import com.vk.api.sdk.queries.wall.WallGetFilter
+import org.telegram.telegrambots.ApiContextInitializer
+import org.telegram.telegrambots.TelegramBotsApi
 import org.telegram.telegrambots.api.methods.send.SendMessage
 import org.telegram.telegrambots.api.objects.Message
+import org.telegram.telegrambots.api.objects.Update
+import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.timer
@@ -47,7 +49,7 @@ fun main(args: Array<String>) {
     VkFetcher(properties.getProperty("vk.appId").toInt(),
             properties.getProperty("vk.token"),
             knownPosts,
-            bot::newPost andThen { _, it -> println(it) })
+            { _: Long, it: String -> println(it) } andThen bot::newPost)
 }
 
 class VkFetcher(appId: Int, token: String, chatToPosts: ChatIdToKnownPosts, private val callback: (Long, String) -> Unit) {
@@ -55,22 +57,53 @@ class VkFetcher(appId: Int, token: String, chatToPosts: ChatIdToKnownPosts, priv
     private val vk = VkApiClient(HttpTransportClient())
 
     init {
-        val convertToText = { post: WallpostFull -> post.text } //todo provide group name, replace links
+        fun convertToText(wallId: String, post: WallpostFull): List<String> {
+            val result = mutableListOf<String>()
+            if (post.text != null && post.text.isNotBlank()) {
+                result.add(post.text.replace(Regex("\\[(.+?)\\|(.+?)]"), "<a href=\"vk.com/$1\">$2</a>"))
+            }
+            if (post.attachments != null && post.attachments.isNotEmpty()) {
+                post.attachments.forEach {
+                    when {
+                        it.photo != null -> with(it.photo) { photo2560 ?: photo1280 ?: photo807 ?: photo604 }?.let { result.add(it) }
+                        it.video != null -> with(it.video) { result.add("vk.com/video${ownerId}_$id") }
+                        it.link != null -> if (post.text?.contains(it.link.url) != true) result.add(it.link.url)
+                        it.audio != null -> result.add("Audio: ${it.audio.artist} - ${it.audio.title}")
+                    }
+                }
+            }
+            return result.map { "from $wallId (added ${LocalDateTime.ofEpochSecond(post.date.toLong(), 0, ZoneOffset.UTC)} UTC):\n\n$it" }
+        }
 
         timer(period = TimeUnit.SECONDS.toMillis(60)) {
-            val wallOwnerIds = chatToPosts.values.flatMap { it.keys }.distinct()
-            println("Going to find new wall messages for $wallOwnerIds")
-            val posts = wallOwnerIds.map {
-                it to vk.wall().get(actor)
-                        .ownerId(it).count(5)
-                        .filter(WallGetFilter.OWNER).execute().items
-            }.toMap()
-            chatToPosts.forEach { (chatId, knownPosts) ->
-                val new = posts
-                        .filterKeys { knownPosts.containsKey(it) }
-                        .mapValues { (key, value) -> value.filter { !knownPosts[key]!!.contains(it.id) } }
-                new.mapValues { it.value.map { it.id } }.forEach { knownPosts[it.key]!!.addAll(it.value) }
-                new.flatMap { it.value }.mapNotNull(convertToText).filter { it.isNotBlank() }.forEach { callback(chatId, it) }
+            try {
+                val wallOwnerIds = chatToPosts.values.flatMap { it.keys }.distinct()
+                println("Going to find new wall messages for $wallOwnerIds")
+                val posts = wallOwnerIds.map {
+                    it to vk.wall().get(actor)
+                            .apply {
+                                if (it.toIntOrNull() != null) {
+                                    ownerId(it.toInt())
+                                } else {
+                                    domain(it)
+                                }
+                            }
+                            .count(5)
+                            .filter(WallGetFilter.OWNER)
+                            .execute().items
+                }.toMap()
+                chatToPosts.forEach { (chatId, knownPosts) ->
+                    val new = posts
+                            .filterKeys { knownPosts.containsKey(it) }
+                            .mapValues { (key, value) -> value.filter { !knownPosts[key]!!.contains(it.id) } }
+                    new.mapValues { it.value.map { it.id } }.forEach { knownPosts[it.key]!!.addAll(it.value) }
+                    new.flatMap { key -> key.value.map { key.key to it } }
+                            .flatMap { convertToText(it.first, it.second) }
+                            .filter { it.isNotBlank() }
+                            .forEach { callback(chatId, it) }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
     }
@@ -84,20 +117,25 @@ class VkFetcherBot(private val token: String,
     override fun getBotUsername() = username
 
     override fun onUpdateReceived(update: Update) {
-        if (sentToMe(update.message) && update.message?.text?.split(" ")?.size == 2) {
-            val text = update.message.text
+        if (sentToMe(update.message) && update.message.hasText()) {
             val chatId = update.message.chatId
-            val groupId = update.message.text.substringAfter(" ")
-            if (text.startsWith("/add")) {
-                println("Add new group ($groupId) for $chatId")
-                val groupToPosts = chatToPosts.getOrPut(chatId, { hashMapOf() })
-                groupToPosts.getOrPut(groupId.toInt(), { mutableListOf() })
-                execute(SendMessage(chatId, "Group $groupId added."))
-            } else if (text.startsWith("/remove")) {
-                println("Remove group $groupId from $chatId")
-                val groupToPosts = chatToPosts[chatId]
-                groupToPosts?.remove(groupId.toInt())
-                execute(SendMessage(chatId, "Group $groupId removed."))
+            val text = update.message.text
+            if (text.split(" ").size == 2) {
+                val groupId = text.substringAfter(" ")
+                if (text.startsWith("/add")) {
+                    println("Add new group ($groupId) for $chatId")
+                    val groupToPosts = chatToPosts.getOrPut(chatId, { hashMapOf() })
+                    groupToPosts.getOrPut(groupId, { mutableListOf() })
+                    execute(SendMessage(chatId, "Group $groupId added."))
+                } else if (text.startsWith("/remove")) {
+                    println("Remove group $groupId from $chatId")
+                    val groupToPosts = chatToPosts[chatId]
+                    groupToPosts?.remove(groupId)
+                    execute(SendMessage(chatId, "Group $groupId removed."))
+                }
+            }
+            if (text.startsWith("/list")) {
+                execute(SendMessage(chatId, chatToPosts[chatId]?.keys?.joinToString(", ") ?: "No walls found for you"))
             }
         }
 
@@ -106,7 +144,7 @@ class VkFetcherBot(private val token: String,
     private fun sentToMe(message: Message?) = message != null &&
             (message.isUserMessage || message.text?.contains("@$username") == true)
 
-    fun newPost(chatId: Long, text: String): Message = execute(SendMessage(chatId, text))
+    fun newPost(chatId: Long, text: String): Message = execute(SendMessage(chatId, text).apply { setParseMode("html") })
 }
 
 infix fun <T, R> ((T, R) -> Any?).andThen(and: (T, R) -> Any?): (T, R) -> Unit = { f, s ->
@@ -114,4 +152,4 @@ infix fun <T, R> ((T, R) -> Any?).andThen(and: (T, R) -> Any?): (T, R) -> Unit =
     and(f, s)
 }
 
-typealias ChatIdToKnownPosts = MutableMap<Long, MutableMap<Int, MutableList<Int>>>
+typealias ChatIdToKnownPosts = MutableMap<Long, MutableMap<String, MutableList<Int>>>
